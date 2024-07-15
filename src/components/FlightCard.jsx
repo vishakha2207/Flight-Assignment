525b2c73115b0ba21e96e40dc4fbb8f048a57c74
vishakha_jogdand <115106919+vishakha2207@users.noreply.github.com>
2024-07-15 17:48:03 +0530

    css

--- src/components/FlightCard.jsx
+++ src/components/FlightCard.jsx
@@ -25,32 +25,40 @@
 
 import React from 'react';
 
-const FlightCard = ({ result, origin, destination}) => {
+const FlightCard = ({ result, src, dest}) => {
   return (
-    <div className="bg-green-900 p-4 rounded-lg shadow-lg m-2 w-64 text-white">
-      <div className="flex items-center justify-center mb-2">
-        {/* <img src={airline.logo} alt={airline.name} className="h-8 w-auto" /> */}
+    <div className="bg-green-900 p-2 rounded-lg w-64 text-white space-x-2">
+      <div className="flex items-center justify-center bg-green-900 p-2">
+        <img src='' alt='logo' className="h-8 w-auto bg-transparent" />
       </div>
-      <h3 className="text-lg font-semibold">{result.partner_program}</h3>
-      <p className="text-sm">{origin} ➔ {destination}</p>
-      <p className="text-sm">2024-07-09-2024-10-07</p>
-      <div className="mt-4">
-        {result.min_business_miles === null ? <p>N/A</p> :
-            <div>
-                {result.min_business_miles} + ${result.min_business_tax}
+      <div className=' bg-green-900 space-y-2'>
+      <h3 className="text-2xl font-semibold  bg-green-900">{result.partner_program}</h3>
+      <p className="text-sm  bg-green-900">{src} ➔ {dest}</p>
+      <p className="text-sm  bg-green-900">2024-07-09-2024-10-07</p>
+      <div className="mt-4  bg-green-900">
+        {result.min_business_miles === null ? <p className='bg-green-900 text-3xl font-bold py-1'>N/A</p> :
+            <div className=' bg-green-900'>
+              <span className='text-3xl bg-green-900 font-bold'>{result.min_business_miles}</span>
+                 + ${result.min_business_tax}
             </div>}
         
-        <p className="text-xs">Min Business Miles</p>
-        {result.min_economy_miles === null ? <p>N/A</p> :
-            <div>
-                {result.min_economy_miles} + ${result.min_economy_tax}
+        <p className="text-xs  bg-green-900">Min Business Miles</p>
+        <div className='py-3 bg-green-900'>
+        {result.min_economy_miles === null ? <p className='bg-green-900 text-3xl font-bold mt-5'>N/A</p> :
+            <div className='bg-green-900'>
+              <span className='text-3xl bg-green-900 font-bold'>{result.min_economy_miles} </span>
+                + ${result.min_economy_tax}
             </div>}
-        <p className="text-xs">Min Economy Miles</p>
-        {result.min_first_miles === null ? <p>N/A</p> :
+        <p className="text-xs  bg-green-900">Min Economy Miles</p>
+        </div>
+        
+        {result.min_first_miles === null ? <p className='bg-green-900 text-2xl font-bold'>N/A</p> :
             <div>
-                {result.min_first_miles} + ${result.min_first_tax}
+              <span className='text-3xl font-bold'>{result.min_first_miles} </span>
+                + ${result.min_first_tax}
             </div>}
-        <p className="text-xs">Min First Miles</p>
+        <p className="text-xs  bg-green-900">Min First Miles</p>
+        </div>
       </div>
     </div>
   );
--- src/components/FlightSearchForm.jsx
+++ src/components/FlightSearchForm.jsx
@@ -9,7 +9,9 @@ const FlightSearchForm = () => {
   const [cabin, setCabin] = useState('Business');
   const [showProFilters, setShowProFilters] = useState(false);
   const [searchResults, setSearchResults] = useState([]);
-
+  const [isSearch, setIsSearch] = useState(false)
+  const [src, setSrc] = useState("")
+  const [dest, setDest] = useState("")
  
 
   const handleSearch = async () => {
@@ -48,6 +50,9 @@ const FlightSearchForm = () => {
       const response = await axios.post('https://cardgpt.in/apitest', json_data, { headers });
       console.log(response)
       setSearchResults(response.data.data);
+      setSrc(origin)
+      setDest(destination)
+      setIsSearch(true)
      
     } catch (error) {
       console.error('Error fetching the data', error);
@@ -55,15 +60,16 @@ const FlightSearchForm = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-green-950">
+    <div className='flex flex-col justify-center items-center'>
+    <div className=" w-1/3 flex-col items-center justify-center bg-green-950">
       <div className=" p-6 rounded-lg shadow-lg">
         <h2 className="text-white text-2xl mb-4 ">Choose Origin & Destination Airports:</h2>
-        <div className="mb-4 bg-black  text-center">
-          <label className="block text-gray-300 text-left pl-3">Origin</label>
+        <div className="mb-4 bg-black">
+          <label className="block  bg-black text-gray-300 text-left pl-3">Origin</label>
           <select
             // value={origin}
             onChange={(e) => setOrigin(e.target.value)}
-            className="w-full mt-2 p-2 rounded  bg-black text-white  hover:bg-gray-600"
+            className="w-full mt-2 p-2 rounded bg-black text-white  hover:bg-gray-600"
           >
             <option value="SYD">SYD</option>
             <option value="DEL">DEL</option>
@@ -73,7 +79,7 @@ const FlightSearchForm = () => {
           </select>
         </div>
         <div className="mb-4  bg-black">
-          <label className="block text-left pl-3 text-gray-300">Destination</label>
+          <label className="block  bg-black text-left pl-3 text-gray-300">Destination</label>
           <select
             // value={destination}
             onChange={(e) => setDestination(e.target.value)}
@@ -87,7 +93,7 @@ const FlightSearchForm = () => {
           </select>
         </div>
         <div className="mb-4  bg-black">
-          <label className="block text-left pl-3 text-gray-300">Cabin Selection</label>
+          <label className="block  bg-black text-left pl-3 text-gray-300">Cabin Selection</label>
           <select
             // value={cabin}
             onChange={(e) => setCabin(e.target.value)}
@@ -99,14 +105,14 @@ const FlightSearchForm = () => {
           </select>
         </div>
         <div className="mb-4 bg-black p-3">
-          <label className="flex items-center space-x- text-gray-300">
+          <label className="flex items-center space-x- bg-black text-gray-300">
             {/* <input
               checked={showProFilters}
               onChange={() => setShowProFilters(!showProFilters)}
               className="form-checkbox h-5 w-5 text-orange-600"
             /> */}
             <FaToggleOn/>
-            <span className="ml-2 ">
+            <span className="ml-2 bg-black">
             
             Show Pro Filters</span>
           </label>
@@ -119,14 +125,17 @@ const FlightSearchForm = () => {
           Search
         </button>
         </div>
-        <div className="flex flex-wrap justify-center">
-          {searchResults.length === 0
-            ? <div>There are no flights</div>
+    
+      </div>
+      
+    </div>
+    <div className="flex justify-center w-full space-x-4">
+      {searchResults.length === 0
+            ?  isSearch === false ? <div></div> : <div>There are no flights</div>
             : searchResults.map((result, index) => (
-                <FlightCard key={index} origin={origin}  destination={destination} result={result} />
+                <FlightCard key={index} src={src}  dest={dest} result={result} />
               ))}
         </div>
-      </div>
     </div>
   );
 };
